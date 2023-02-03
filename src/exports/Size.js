9d0f3856275746d22979792fa02cb25fe5447eb5
Neloxe <adrienverhaeghe@gmail.com>
2023-02-03 09:19:11 +0100

    build

--- src/exports/Size.js
+++ src/exports/Size.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-tabs */
 export const SIZE = {
-  xl: 72,
-  l: 60,
-  m: 54,
-  s: 48,
-  xs: 38,
-  xxs: 36,
-  xxxs: 33
+	xl: 72,
+	l: 60,
+	m: 54,
+	s: 48,
+	xs: 38,
+	xxs: 36,
+	xxxs: 33
 }
